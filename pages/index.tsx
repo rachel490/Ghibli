be304c4f85e9de1e5133ca6ea3eabcb7272a4758
rachel490 <ccori12@naver.com>
2023-06-23 14:28:06 +0900

    feature: Finish Homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from "next";
+import CardList from '../components/common/CardList/CardList';
+import ImageCarousel from "../components/main/ImageCarousel/ImageCarousel";
+import SearchBar from '../components/main/SearchBar/SearchBar';
+import Layout from "../components/common/Layout/Layout";
 
 const Home: NextPage = () => {
-  return <div>Hello World</div>;
+  return (
+    <Layout >
+      <ImageCarousel />
+      <SearchBar />
+      <CardList />
+    </Layout>
+  );
 };
 
 export default Home;
