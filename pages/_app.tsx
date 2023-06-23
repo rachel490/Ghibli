a4f960754a7b90214c1cbc90a9b5d8a48835741b
rachel490 <ccori12@naver.com>
2023-06-23 16:36:27 +0900

    refactor: Change apollo client uri to environment varaible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,17 @@
 import type { AppProps } from "next/app";
-import { ApolloProvider } from '@apollo/client';
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 import "../styles/globals.css";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
-  cache: new InMemoryCache()
+  uri: `${process.env.NEXT_PUBLIC_URL}`,
+  cache: new InMemoryCache(),
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
-        <Component {...pageProps} />
+      <Component {...pageProps} />
     </ApolloProvider>
   );
 }
