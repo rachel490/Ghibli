2350fa21cc16ed72e6b83b681a433ff1b9ce188b
rachel490 <ccori12@naver.com>
2023-06-23 14:25:16 +0900

    feature: add apollo client provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import type { AppProps } from "next/app";
-
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client'
 import "../styles/globals.css";
-import Layout from "../components/common/Layout/Layout";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/api/graphql',
+  cache: new InMemoryCache()
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={client}>
+        <Component {...pageProps} />
+    </ApolloProvider>
   );
 }
 
