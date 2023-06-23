4b7d79c63fb4c50fd8f7237cc0ad74ec7adb6e6d
rachel490 <ccori12@naver.com>
2023-06-23 14:19:52 +0900

    fix: change return value of resolvers due to api change

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -14,41 +14,41 @@ const resolvers = {
       return parent.people.map(async (person: string) => {
         const person_id = person.split("people/")[1];
         const person_data = await axios.get(PERSON_API(person_id));
-        return person_data.data;
+        return person_data.data.data;
       });
     },
     species: (parent: any) => {
       return parent.species.map(async (species: string) => {
         const species_id = species.split("species/")[1];
         const species_data = await axios.get(SPECIES_API(species_id));
-        return species_data.data;
+        return species_data.data.data;
       });
     },
   },
   Query: {
     films: async () => {
       const res = await axios.get(FILMS_API);
-      return res.data;
+      return res.data.data;
     },
     film: async (_: any, { id }: { id: string }) => {
       const res = await axios.get(FILM_API(id));
-      return res.data;
+      return res.data.data;
     },
     people: async () => {
       const res = await axios.get(PEOPLE_API);
-      return res.data;
+      return res.data.data;
     },
     person: async (_: any, { id }: { id: string }) => {
       const res = await axios.get(PERSON_API(id));
-      return res.data;
+      return res.data.data;
     },
     all_species: async () => {
       const res = await axios.get(SPECIES_ALL_API);
-      return res.data;
+      return res.data.data;
     },
     species: async (_: any, { id }: { id: string }) => {
       const res = await axios.get(SPECIES_API(id));
-      return res.data;
+      return res.data.data;
     },
   },
 };
