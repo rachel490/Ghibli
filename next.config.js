bc1232dc374449adc9e46a36e2ad5b28cf33c61a
rachel490 <ccori12@naver.com>
2023-06-23 15:30:33 +0900

    chore: add image domains in next config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["www.ghibli.jp", "www.pixelstalk.net"],
+    domains: ["www.ghibli.jp", "www.pixelstalk.net", "image.tmdb.org", "www.themoviedb.org", "e1.pxfuel.com"],
   },
 };
 
